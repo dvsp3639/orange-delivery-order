f9de1c88a83c8c5e5d6c1766c94f68a391a87736
dvsp3639 <prasad.dam30@gmail.com>
2026-02-13 22:52:55 +0530

    Update payment.js

--- payment.js
+++ payment.js
@@ -44,7 +44,7 @@ async function initiatePhonePePayment(totalAmount) {
             message: "Orange Delivery Payment",
 
             merchantUrls: {
-              redirectUrl: "https://yourdomain.com/success.html",
+              redirectUrl: "https://order.orange-foods,info",
             },
           },
         }),
